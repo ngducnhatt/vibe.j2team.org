94f44954652d383ab695e485c25293901d959501
Nguyen Duc Nhat <nhaatjisme@gmail.com>
2026-03-10 02:26:09 +0700

    fix: meta.ts error category

--- src/views/cuoc-song-hang-ngay-cua-ai/meta.ts
+++ src/views/cuoc-song-hang-ngay-cua-ai/meta.ts
@@ -1,11 +1,11 @@
-import type { PageMeta } from '@/types/page'
+import type { PageMeta } from "@/types/page";
 
 const meta: PageMeta = {
-  name: 'Cuộc sống hàng ngày của AI',
-  description: 'Mô tả cuộc sống của AI bằng giao diện pixel.',
-  author: 'ngducnhatt',
-  facebook: 'https://github.com/ngducnhatt',
-  category: 'fun',
-}
+  name: "Cuộc sống hàng ngày của AI",
+  description: "Mô tả cuộc sống của AI bằng giao diện pixel.",
+  author: "ngducnhatt",
+  facebook: "https://github.com/ngducnhatt",
+  category: "fun",
+};
 
-export default meta
+export default meta;
